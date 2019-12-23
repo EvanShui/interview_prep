74322cf17a328c18e8456151a2a839cd2186f822
EvanShui <evanshui0@gmail.com>
2019-12-22 23:35:14 -0800

    dnc lomuto partitioning

---
+++ divide_conquer/lomuto.py
@@ -0,0 +1,17 @@
+def partition(A):
+    p = A[0]
+    s = 0
+    for i in range(1, len(A)):
+        if A[i] < p:
+            s += 1
+            A[s], A[i] = A[i], A[s]
+    A[0],A[s] = A[s],A[0]
+    return A
+
+def main():
+    A = [4,1,10,8,7,12,9,2,15]
+    A = partition(A)
+    assert(A == [2,1,4,8,7,12,9,10,15])
+
+if __name__ == '__main__':
+    main()
